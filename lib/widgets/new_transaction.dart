c4ef231c0f8a718b9eb77281063fff15ec483551
snarksliveshere <snarksliveshere@gmail.com>
2019-09-15 12:53:37 +0300

    fixes before

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
+
   final Function _addTx;
-  final _titleController = TextEditingController();
-  final _amountController = TextEditingController();
 
   NewTransaction(this._addTx);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   void submitData() {
     final enteredTitle = _titleController.text;
@@ -15,7 +22,8 @@ class NewTransaction extends StatelessWidget {
       return;
     }
 
-    _addTx(enteredTitle, enteredAmount);
+    widget._addTx(enteredTitle, enteredAmount);
+    Navigator.of(context).pop();
   }
 
   @override
