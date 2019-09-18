dfab143554505b0a4b8921f30b5d3cc5a10cb010
snarksliveshere <snarksliveshere@gmail.com>
2019-09-18 20:08:57 +0300

    flexible appearance of button icon

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -61,11 +61,18 @@ class TransactionList extends StatelessWidget {
                     subtitle: Text(
                       DateFormat.yMMMd().format(transactions[index].date),
                     ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () => this.deleteTx(transactions[index].id),
-                    ),
+                    trailing: MediaQuery.of(context).size.width > 360
+                        ? FlatButton.icon(
+                            onPressed: () => this.deleteTx(transactions[index].id),
+                            textColor: Theme.of(context).errorColor,
+                            icon: Icon(Icons.delete),
+                            label: Text('Delete'),
+                          )
+                        : IconButton(
+                            icon: Icon(Icons.delete),
+                            color: Theme.of(context).errorColor,
+                            onPressed: () => this.deleteTx(transactions[index].id),
+                          ),
                   ),
                 );
               },
